a36a5b90d2fcc580188fa30eceea5d4cd224889f
asim-p <pokharel.asim@gmail.com>
2025-06-20 15:44:12 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const  {Server} = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+
+//Sockets
+const io = new Server(server); 
+
+io.on('connection',socket=>{
+    socket.on('chat-message',(message,userid) => {
+        io.emit('chat-message',message);
+        io.emit('userid',userid);
+    })
+})
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    return res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+server.listen(3000,()=>console.log("listening"));
