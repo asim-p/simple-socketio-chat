617a84f7ce1b09a6fd8aad573a704a327d9d6260
asim-p <pokharel.asim@gmail.com>
2025-06-23 14:55:05 +0545

    Disconnect Button Added

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const http = require('http');
-const  {Server} = require('socket.io');
+const {Server} = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-
-//Sockets
-const io = new Server(server); 
-
-io.on('connection',socket=>{
-    socket.on('chat-message',(message) => {
-        io.emit('chat-message',message);
-    })
-})
+const io = new Server(server,{
+    connectionStateRecovery:{}
+}); 
 
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     return res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
+
+io.on('connection',socket=>{
+    socket.on('chat-message',msg => {
+        io.emit('chat-message',msg);
+    });
+});
 
 server.listen(3000,()=>console.log("listening"));
